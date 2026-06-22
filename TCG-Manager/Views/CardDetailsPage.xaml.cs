76218a2a38e2a77083ac7f6ae03a676296f2b5c4
Haydn Tanner <haydnt15@gmail.com>
2026-06-22 21:36:23 +0930

    Added formatting to the price grid on the detailed card page.

--- TCG-Manager/Views/CardDetailsPage.xaml.cs
+++ TCG-Manager/Views/CardDetailsPage.xaml.cs
@@ -24,21 +24,41 @@ public partial class CardDetailsPage : ContentPage
 		ExtractedData data = extractor.Extract(card);
         Dictionary<string, string> GradePrices = data.GetGradePrices();
 
-        int counter = 0;
+
+        int column = 0;
+        int rowOffset = 0;
         GridLength width = 100;
         foreach (var k in data.GetGradePrices().Keys)
         {
             AveragePriceGrid.AddColumnDefinition(new ColumnDefinition(width));
             Label key = new Label();
+            Thickness margin = new Thickness(5, 10, 5, 0);
+            key.Margin = margin;
+            key.BackgroundColor = new Color(0.75f);
+            key.TextColor = new Color(0, 0, 0);
+
             Label value = new Label();
+            value.Margin = margin;
+            value.BackgroundColor = new Color(0.25f);
+            value.TextColor = new Color(0, 0, 0);
 
             key.Text = k;
             value.Text = GradePrices[k];
-            AveragePriceGrid.Add(key, counter, 0);
-            AveragePriceGrid.Add(value, counter, 1);
+            AveragePriceGrid.Add(key, column, 0 + rowOffset);
+            AveragePriceGrid.Add(value, column, 1 + rowOffset);
 
-            counter++;
+            column++;
+
+            if (column == 5)
+            {
+                column = 0;
+                AveragePriceGrid.AddRowDefinition(new RowDefinition());
+                AveragePriceGrid.AddRowDefinition(new RowDefinition());
+                rowOffset += 2;
+            }
         }
+
+
     }
 
     private void UpdateCard(int id)
